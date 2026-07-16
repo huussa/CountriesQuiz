759c922623fb9a672ed417303dc596b6de4c059c
Hussam Alghamdi <hussam@example.com>
2026-07-16 09:29:12 +0300

    fix bugs in responsive design

--- src/app/AllCountiresGame/components/question-card.js
+++ src/app/AllCountiresGame/components/question-card.js
@@ -1,5 +1,5 @@
 import styles from "../page.module.css";
-import ProgressBar from './progress-bar'
+import ProgressBar from "./progress-bar";
 import Timer from "./timer";
 import { useTranslation } from "react-i18next";
 
@@ -16,7 +16,7 @@ function QuestionCard({
   totalSeconds,
 }) {
   const { t, i18n } = useTranslation();
-  const dir = i18n.language
+  const dir = i18n.language;
 
   const getButtonClass = (opt) => {
     if (!isAnswered) return "";
@@ -25,30 +25,40 @@ function QuestionCard({
     return "";
   };
   const getCountryName = (country) => {
-    if (i18n.language === 'ar' && country.nameAr) {
+    if (i18n.language === "ar" && country.nameAr) {
       return country.nameAr;
     }
-    return country.name; 
+    return country.name;
   };
 
   return (
     <div className={styles.card}>
-      <div style={{display: streak > 2 ? "block" : "none"}} dir={dir === "ar" ? "rtl" : "ltr"}>{`${t("gamePage.streak")} ${streak}🔥`}</div>
+      <div
+        style={{ display: streak > 2 ? "block" : "none" }}
+        dir={dir === "ar" ? "rtl" : "ltr"}
+        className={styles.streak}
+      >
+        {`${t("gamePage.streak")} ${streak}🔥`}
+      </div>
       <div className={styles.quizInformations}>
-        <ProgressBar currentIndex={currentIndex} totalQuestions={totalQuestions} streak={streak}/>
-        <Timer totalSeconds={totalSeconds}/>
+        <ProgressBar
+          currentIndex={currentIndex}
+          totalQuestions={totalQuestions}
+          streak={streak}
+        />
+        <Timer totalSeconds={totalSeconds} />
       </div>
       <img src={img.src} alt={img.alt} />
       <div className={styles.buttons}>
-      {options.map((opt) => (
-        <button
-          key={opt.code}
-          onClick={() => onAnswer(opt)}
-          className={getButtonClass(opt)}
-        >
-          {getCountryName(opt)}
-        </button>
-      ))}
+        {options.map((opt) => (
+          <button
+            key={opt.code}
+            onClick={() => onAnswer(opt)}
+            className={getButtonClass(opt)}
+          >
+            {getCountryName(opt)}
+          </button>
+        ))}
       </div>
     </div>
   );
